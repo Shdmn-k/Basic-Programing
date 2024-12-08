e58a58f725bd9449acc21172f9de36d779c545ed
Shdmn-k <shadmeenbk@gmail.com>
2024-12-08 22:33:11 +0530

    my first commit :)

---
+++ index.js
@@ -0,0 +1,11 @@
+function checkPrime(num) {
+    if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+  
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false; // If the number is divisible by any number between 2 and num-1, it's not prime
+      }
+    }
+  
+    return true; // If no divisors are found, the number is prime
+  }
